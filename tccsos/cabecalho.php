e9dc9f7407c0d2a5c178cf43e1e1d30a470395c2
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2019-03-29 14:21:03 -0300

    Add files via upload

--- tccsos/cabecalho.php
+++ tccsos/cabecalho.php
@@ -57,7 +57,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item active">
+                <li class="nav-item ">
                     <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
--- tccsos/index.php
+++ tccsos/index.php
@@ -35,6 +35,59 @@ include "cabecalho.php";
         <!-- /END THE FEATURETTES -->
 
     </div><!-- /.container -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""</li>
+    </ol>
+    <div class="carousel-inner" >
+        <div class="carousel-item">
+            <img src="img/cobra3.jpg" class="imagem-carrossel">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
+            <div class="container">
+
+                <div class="carousel-caption text-left">
+                    <h1>Serpente(Nome Científico)</h1>
+                    <p>Descrição Breve da Serpente</p>
+
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item teste active">
+            <img src="img/cobra2.jpg" class="imagem-carrossel">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Resgate#1</h1>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/cobra1.jpg" class="imagem-carrossel">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
+            <div class="container">
+                <div class="carousel-caption text-right">
+                    <h1>Resgate#2</h1>
+
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/cobra1.jpg" class="imagem-carrossel">
+            <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"></rect></svg>
+            <div class="container">
+                <div class="carousel-caption text-right">
+                    <h1>Resgate#3</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr class="featurette-divider">
 
 
 
