a9fe7a6d3e57d40aec5e5acb194df3a76b7d1aae
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2019-05-08 08:48:50 -0300

    Add files via upload

--- tccsos/conferirLogin.php
+++ tccsos/conferirLogin.php
@@ -4,17 +4,17 @@ $email = $_POST['emailLogin'];
 $senha = $_POST['senhaLogin'];
 
 if($email == 'matheuseduardo8000@gmail.com' or $email == 'luisdasilvaguilherme5@gmail.com' && $senha == 'admin'){
-    echo '<h1 class="nomeLogin">Bem Vindo Adiministrador</h1>';
+    echo '<h1 class="msgBemVindo">Bem Vindo Adiministrador</h1>';
     $_SESSION['nome']='Adiministrador';
     $_SESSION['email'] = $_POST['emailLogin'];
     echo '<meta http-equiv="refresh" content="3;url=index.php">';
-}elseif ($login == 'user@gmail.com' && $senha == 'user'){
-    echo '<h1 class="nomeLogin">Bem Vindo Usuario</h1>';
+}elseif($email == 'user@gmail.com' && $senha == 'user'){
+    echo '<h1 class="msgBemVindo">Bem Vindo Usuario</h1>';
     $_SESSION['nome']='Usuario';
     $_SESSION['email'] = $_POST['emailLogin'];
     echo '<meta http-equiv="refresh" content="3;url=index.php">';
-}elseif ($login == 'resgatador@gmail.com' && $senha == 'resgatador') {
-    echo '<h1 class="nomeLogin">Bem Vindo Avaliador</h1>';
+}elseif($email == 'resgatador@gmail.com' && $senha == 'resgatador') {
+    echo '<h1 class="msgBemVindo">Bem Vindo Resgatador</h1>';
     $_SESSION['nome'] = 'Resgatador';
     $_SESSION['email'] = $_POST['emailLogin'];
     echo '<meta http-equiv="refresh" content="3;url=index.php">';
