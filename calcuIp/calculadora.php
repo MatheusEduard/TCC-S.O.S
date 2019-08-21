a335d7e4e39ebcd2033b3c77458abb9a7905c9fe
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2019-08-21 14:28:34 -0300

    Add files via upload

--- calcuIp/calculadora.php
+++ calcuIp/calculadora.php
@@ -14,17 +14,19 @@ ini_set(“display_errors”, 1 );
     <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
-<form method="post" action="calculadora.php?acao=calcular">
-    <label class="titulo" for="ip">Informe o Endereço IP:</label>
-    <br>
-    <input type="text" name="ip">
-    <br>
-    <label class="titulo" for="ip">Informe a Mascara:</label>
-    <br>
-    <input type="text" name="masc">
-    <br>
-    <input type="submit" name="enviar" value="Enviar">
-</form>
+<div class="formu">
+	<form method="post" action="calculadora.php?acao=calcular">
+		<label class="titulo" for="ip">Informe o Endereço IP:</label>
+		<br>
+		<input class="insert1" type="text" name="ip">
+		<br>
+		<label class="titulo2" for="ip">Informe a Mascara:</label>
+		<br>
+		<input class="insert2" type="text" name="masc">
+		<br>
+		<input class="subm" type="submit" name="enviar" value="Enviar">
+	</form>
+</div>
 <?php
 if (isset($_GET['acao']) && $_GET['acao'] == 'calcular' ){
     $valido= verificaIPeMasc($_POST["ip"],$_POST["masc"]);
@@ -39,56 +41,37 @@ if (isset($_GET['acao']) && $_GET['acao'] == 'calcular' ){
     $tipIP=tipIP($_POST["ip"]);
     if($valido=="valor valido"){
 ?>
+<div class="ips">
     <ul style="list-style-type: none;">
-        <li class="titulo">Quantidade de Sub-Redes Que Podem Ser Criadas: <?=$qtdSubRed?></li>
+        <li class="tituloip">Quantidade de Sub-Redes Que Podem Ser Criadas: <?=$qtdSubRed?></li>
     </ul>
     <ul style="list-style-type: none;">
-        <li class="titulo">Quantidade de IPs Host por Sub-Rede: <?=$qtdHost?></li>
+        <li class="tituloip2">Quantidade de IPs Host por Sub-Rede: <?=$qtdHost?></li>
     </ul>
     <ul style="list-style-type: none;">
-        <li class="titulo">Mascara de Sub-rede em Formato Decimal: <?=$mascDec?></li>
+        <li class="tituloip3">Mascara de Sub-rede em Formato Decimal: <?=$mascDec?></li>
     </ul>
     <ul style="list-style-type: none;">
-        <li class="titulo">Clase do IP: <?=$classeIP?></li>
+        <li class="tituloip4">Clase do IP: <?=$classeIP?></li>
     </ul>
     <ul style="list-style-type: none;">
-        <li class="titulo">Tipo de IP: <?=$tipIP?></li>
+        <li class="tituloip5">Tipo de IP: <?=$tipIP?></li>
     </ul>
 
     <ul style="list-style-type: none;">
-        <li class="titulo">IPs de Rede:</li>
+        <li class="tituloip6">IPs das Sub-Redes Gerados:</li>
     <?php
         for($i=0;$i<$qtdSubRed;$i++){
-            echo "<li class='subtitulo'>".$ipRede[$i]."</li>";
+			echo "<li class='subtitulo'> Sub-Rede: ".$i."</li>";
+            echo "<li class='subtitulo2'> IP de Rede: ".$ipRede[$i]."</li>";
+            echo "<li class='subtitulo3'> IP de Broadcast: ".$ipBrod[$i]."</li>";
+            echo "<li class='subtitulo4'> Primeiro IP Host: ".$primHost[$i]."</li>";
+            echo "<li class='subtitulo5'> Ultimo IP Host: ".$ultHost[$i]."</li>";
     }
     ?>
     </ul>
-    <ul style="list-style-type: none;">
-        <li class="titulo">IPs de Broadcast</li>
-    <?php
-        for($i=0;$i<$qtdSubRed;$i++){
-            echo "<li class='subtitulo'>".$ipBrod[$i]."</li>";
-    }
-    ?>
-    </ul>
-    <ul style="list-style-type: none;">
-        <li class="titulo">Primeiro IP Host Por Sub-Rede:</li>
-    <?php
-        for($i=0;$i<$qtdSubRed;$i++){
-            echo "<li class='subtitulo'>".$primHost[$i]."</li>";
-    }
-    ?>
-    </ul>
-    <ul style="list-style-type: none;">
-        <li class="titulo">Ultimo IP Host Por Sub-Rede:</li>
-    <?php
-        for($i=0;$i<$qtdSubRed;$i++){
-            echo "<li class='subtitulo'>".$ultHost[$i]."</li>";
-        }
-    ?>
-    </ul>
-    <?php
- 
+</div>
+  <?php
 }else{
         echo"Algum Valor Inserido É Inválido!!";
         echo '<meta http-equiv="refresh" content="4;url=calculadora.php">'; 
